1027418f4e73a1e42b7cab86f0b6052b7054c3de
Dion Purushotham <mail@dion.software>
2026-05-21 12:31:02 -0400

    feat: add AgentSeed favicon assets

--- app/app.vue
+++ app/app.vue
@@ -97,6 +97,23 @@ useHead({
 	},
 	link: [
 		{
+			rel: 'icon',
+			type: 'image/png',
+			sizes: '32x32',
+			href: '/favicon-32x32.png'
+		},
+		{
+			rel: 'icon',
+			type: 'image/png',
+			sizes: '16x16',
+			href: '/favicon-16x16.png'
+		},
+		{
+			rel: 'alternate icon',
+			type: 'image/x-icon',
+			href: '/favicon.ico'
+		},
+		{
 			rel: 'canonical',
 			href: siteUrl
 		}
